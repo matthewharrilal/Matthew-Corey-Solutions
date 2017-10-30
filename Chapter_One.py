d47649b10b3113e28130f312b3c6c21e45f7c6d6
matthewharrilal <matthewharrilal@gmail.com>
2017-10-30 08:11:31 -0700

    Implemented the comparing of permutations still have to polish

--- Chapter_One.py
+++ Chapter_One.py
@@ -1,10 +1,22 @@
-# The first chapter of the crackin the coding interview questions
-from itertools import permutations
 
-def permutations_method(word):
-    perms = [''.join(p) for p in permutations(word)]
+'''Given two strings write a method to decide if one is a permutation of the other'''
+
+def compare_two_words(word1, word2):
+    word1_dictionary = {}
+    word2_dictionary = {}
+    for letter in word1:
+        word1_dictionary[letter] = word1.count(letter)
+
+    for letter in word2:
+        word2_dictionary[letter] = word2.count(letter)
+
+    if word1_dictionary.keys()  == word2_dictionary.keys():
+       if word1.count(letter) == word2.count(letter):
+           return "The words meet the frequency"
+       else:
+           return "The words do not meet the frequency"
+    else:
+        return "The words do not have the same letters"
 
 
-
-# Essentially what is happening is that we are checking if we are given two words if one of the words are a permutation of the other therefore
-# we essentially compare all the permutations
+print(compare_two_words("iceman", "cinema"))
